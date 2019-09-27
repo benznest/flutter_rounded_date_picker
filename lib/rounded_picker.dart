c3d3367f5bda6b3771ab5f65b7a9b2262d1050a0
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-09-27 17:45:39 +0200

    Add possibility to add a custom left button and change action button text

---
+++ lib/rounded_picker.dart
@@ -0,0 +1,6 @@
+library flutter_rounded_date_picker;
+
+export './flutter_rounded_date_picker_widget.dart' show showRoundedDatePicker;
+export './flutter_rounded_time_picker_widget.dart' show showRoundedTimePicker;
+export './cupertino_rounded_date_picker.dart' show CupertinoRoundedDatePicker;
+export './cupertino_rounded_duration_picker.dart' show CupertinoRoundedDurationPicker;
