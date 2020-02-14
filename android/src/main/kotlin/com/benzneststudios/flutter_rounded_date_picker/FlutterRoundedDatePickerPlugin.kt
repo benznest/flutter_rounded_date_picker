59528736aab248d1f3172312b6087c3992047193
Benznest p <benz7net@gmail.com>
2020-02-14 19:20:44 +0700

    edit readme.

---
+++ android/src/main/kotlin/com/benzneststudios/flutter_rounded_date_picker/FlutterRoundedDatePickerPlugin.kt
@@ -0,0 +1,25 @@
+package com.benzneststudios.flutter_rounded_date_picker
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FlutterRoundedDatePickerPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "flutter_rounded_date_picker")
+      channel.setMethodCallHandler(FlutterRoundedDatePickerPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
