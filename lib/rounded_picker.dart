b01c290f41ea3d41b1b62c9baca0682687a5be81
nohli <43643339+nohli@users.noreply.github.com>
2020-08-11 18:37:08 +0200

    Add import for MaterialRoundedYearPickerStyle

--- lib/rounded_picker.dart
+++ lib/rounded_picker.dart
@@ -5,4 +5,5 @@ export './src/flutter_rounded_time_picker_widget.dart' show showRoundedTimePicke
 export './src/cupertino_rounded_date_picker.dart' show CupertinoRoundedDatePicker;
 export './src/cupertino_rounded_duration_picker.dart' show CupertinoRoundedDurationPicker;
 export './src/material_rounded_date_picker_style.dart' show MaterialRoundedDatePickerStyle;
+export './src/material_rounded_year_picker_style.dart' show MaterialRoundedYearPickerStyle;
 export './src/era_mode.dart' show EraMode;
